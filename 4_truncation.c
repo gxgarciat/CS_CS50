c50057ca9d4327a9f5943366d611b7be61def3ce
gxgarciat <info@gxgarciat.dev>
2021-10-02 14:51:49 +0100

    Coding

---
+++ 4_truncation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  // Get numbers from user
+  int x = get_int("x: ");
+  int y = get_int("y: ");
+
+  // Calculation & printing out
+  float z = (float) x / (float) y;
+  printf("%f \n", z);
+
+}
