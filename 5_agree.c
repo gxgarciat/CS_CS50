8eb77c000f12b41581e492d7b7a6e114991e03ab
gxgarciat <info@gxgarciat.dev>
2021-10-04 20:44:11 +0100

    Coding

---
+++ 5_agree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  // Get input from user
+  char c == get_char("Do you agree: ");
+  if (c == "y")
+  {
+    printf("Agreed. \n");
+  }
+  else if (c == "n")
+  {
+    printf("Not agreed. \n");
+  }
+
+}
